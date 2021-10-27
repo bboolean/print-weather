a28abefba63aee19087a8266be7e9792d81af97f
benjamin <benjamin@localhost.localdomain>
2021-10-26 20:18:51 -0400

    Update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject print-weather "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Print the weather for a specific latitude and longitude."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "2.4.0"]
